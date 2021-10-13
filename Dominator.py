1ab9c5eb8b1675ac72c0b3005b966f3ceccdadfe
mrscour <71791944+mrscour@users.noreply.github.com>
2021-10-14 01:24:03 +0300

    Add files via upload

---
+++ Dominator.py
@@ -0,0 +1,39 @@
+'''Task description
+An array A consisting of N integers is given. The dominator of array A is the value that occurs in more than half of the elements of A.
+
+For example, consider array A such that
+
+ A[0] = 3    A[1] = 4    A[2] =  3
+ A[3] = 2    A[4] = 3    A[5] = -1
+ A[6] = 3    A[7] = 3
+The dominator of A is 3 because it occurs in 5 out of 8 elements of A (namely in those with indices 0, 2, 4, 6 and 7) and 5 is more than a half of 8.
+
+Write a function
+
+def solution(A)
+
+that, given an array A consisting of N integers, returns index of any element of array A in which the dominator of A occurs. The function should return −1 if array A does not have a dominator.
+
+For example, given array A such that
+
+ A[0] = 3    A[1] = 4    A[2] =  3
+ A[3] = 2    A[4] = 3    A[5] = -1
+ A[6] = 3    A[7] = 3
+the function may return 0, 2, 4, 6 or 7, as explained above.
+
+Write an efficient algorithm for the following assumptions:
+
+N is an integer within the range [0..100,000];
+each element of array A is an integer within the range [−2,147,483,648..2,147,483,647].'''
+
+# Complexity O(N*log(N)) or O(N)
+# https://app.codility.com/demo/results/training76RPJP-334/
+
+def solution(A):
+    N = len(A)
+    counter = {}
+    for i in A:
+        counter[i] = counter.setdefault(i, 0) + 1 # return current value or 0, then incrementing it by 1
+        if counter[i] > N // 2:
+            return A.index(i)
+    return -1
